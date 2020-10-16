cb2204763b4b6fcf92a27b3a0f2ce04202f266f1
surya <bittergourd76@gmail.com>
2020-10-16 15:12:26 +0530

    Added client.js

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -4,7 +4,8 @@ import React from 'react';
 const Home = () => {
     return(
         <div>
-            I'm the home component
+            I'm the very very BEST home component
+            <button onClick={() => console.log('Hi there!')}>press me</button>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-const express = require('express');
-const React = require('react');
-const renderToStrings = require('react-dom/server').renderToString;
-const Home = require('./client/components/Home').default;
+import express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import Home from './client/components/Home';
+
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    const content = renderToStrings(<Home />);
-    res.send(content);
+    const content = renderToString(<Home />);
+
+    const html = `
+    <html>
+        <head></head>
+        <dody>
+            <div id="root">${content}</div>
+            <script src="bundle.js"></script>
+        </body>
+    </html>
+    `;
+    res.send(html);
 });
 
 app.listen(3000);
