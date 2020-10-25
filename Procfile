1bf68b395e681a25169ff97e53a4950e0e66881b
surya <bittergourd76@gmail.com>
2020-10-25 18:23:07 +0530

    bug fix

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:node src/index.js
+web:node node build/bundle.js
--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -1,6 +1,7 @@
 
 export const FETCH_USERS = 'fetch_users';
 export const FETCH_CURRENT_USER = 'fetch_current_user';
+export const FETCH_ADMINS
 
 export const fetchUser = () => async (dispatch, getState, api) => {
     const res = await api.get('/users');
@@ -11,3 +12,5 @@ export const fetchCurrentUser = () => async (dispatch, getState, api) => {
     const res = await api.get('/current_user');
     dispatch( { type: FETCH_CURRENT_USER, payload: res } );
 };
+
+//export const fetchAdmins = () => async ()
